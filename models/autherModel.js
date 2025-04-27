8ac85ffad038ecea28edc345ab879b170d89c121
Utplaksh Mishra <utplakshmishra@gmail.com>
2025-04-27 12:29:54 +0530

    Added authers model Schema to code base and pased auther argument to routes

---
+++ models/autherModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+// What is Schema?
+// in MongoDB and non-sequal database the schema is like the table
+
+// building schema
+const autherData = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+});
+
+const autherSchema = mongoose.model("Auther", autherData);
+
+
+export default autherSchema;
--- routes/autherRoutes.js
+++ routes/autherRoutes.js
@@ -1,19 +1,21 @@
 import express from "express";
-const router = express.Router()
+const router = express.Router();
+import Auther from "../models/autherModel.js";
 
 // All auther routes
-router.get('/',(req,res)=>{
-    res.render('authers/autherIndex')
-})
+router.get("/", (req, res) => {
+  res.render("authers/autherIndex");
+});
 
 // New Auther Routes
-router.get('/new',(req,res)=>{
-    res.render('authers/autherNew')
-})
+router.get("/new", (req, res) => {
+  res.render("authers/autherNew", { auther: new Auther() });
+});
 
 // Creating new auther routes
-router.post('/',(req,res)=>{
-    res.send('Create')
-})
+router.post("/", (req, res) => {
+  res.send("Create");
+  console.log(`Creating auther name and saving it to database`);
+});
 
-export default router
+export default router;
