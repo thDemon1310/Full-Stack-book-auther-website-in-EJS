cc3879ead0664893e0255d0e262f909d142a7eb2
Utplaksh Mishra <utplakshmishra@gmail.com>
2025-04-28 21:20:25 +0530

    added code that will get data from the DB and list it on /Authers/ url

--- routes/autherRoutes.js
+++ routes/autherRoutes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 import Auther from "../models/autherModel.js";
 
 // All auther routes
-router.get("/", (req, res) => {
-  res.render("authers/autherIndex");
+router.get("/", async (req, res) => {
+  try {
+    const dbAutherList = await Auther.find({});
+    res.render("authers/autherIndex", { authersList: dbAutherList });
+  } catch (error) {
+    console.error(error);
+    res.redirect("/");
+  }
 });
 
 // New Auther Routes
@@ -18,7 +24,7 @@ router.post("/", async (req, res) => {
     name: req.body.name,
   });
   try {
-    const newAuther =await auther.save();
+    const newAuther = await auther.save();
     console.log(`The name save to database`);
     res.redirect("authers");
     // res.redirect(`authers/${newAuther.id}`)
