befc5c3f3b6aabafabdff733fb025fb597770d3e
Utplaksh Mishra <utplakshmishra@gmail.com>
2025-05-10 02:27:09 +0530

    made ejs for books making form for adding books title authes option added to code base

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,21 +1,33 @@
 import express from "express";
 import Book from "../models/bookModel.js";
+import Auther from "../models/autherModel.js";
 const router = express.Router();
 
 // All book routes
 router.get("/", async (req, res) => {
-    res.send('Book index page')
+  res.render("books/bookIndex");
 });
 
 // New Book routes
-router.get("/new", (req, res) => {
-    res.send('New Book')
+router.get("/new", async (req, res) => {
+  try {
+    const authers = await  Auther.find({});
+    const book = new Book();
+    res.render("books/bookNew", {
+      authersList: authers,
+      bookList: book,
+    });
+  } catch (error) {
+    console.error(error);
+    res.redirect("/books/")
+    
+  }
 });
 
 // Create Book Routes
 router.post("/", async (req, res) => {
-    res.send('Create Book')
-    console.log('post request from client for books')
+  res.send("Create Book");
+  console.log("post request from client for books");
 });
 
 export default router;
