3d10de2ffa68559ee8a0bb343807b70b87f0de52
Utplaksh Mishra <umishra8770@gmail.com>
2025-06-03 20:51:56 +0530

    added favicon.ico to website and formated server.js

--- server.js
+++ server.js
@@ -5,37 +5,36 @@ if (process.env.NODE_ENV !== "production") {
 
 import express from "express";
 import bodyParser from "body-parser";
-const app = express();
-const hostname = process.env.HOST;
-const port = process.env.PORT;
 import expressEjsLayouts from "express-ejs-layouts";
-import { fileURLToPath } from "url";
+import mongoose from "mongoose";
 import path from "path";
+import { fileURLToPath } from "url";
+
 // importing routes
 import indexRouter from "./routes/indexRoute.js";
 import autherRoutes from "./routes/autherRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
+
+const app = express();
+const hostname = process.env.HOST || "localhost";
+const port = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set("layout", "layouts/layout"); // hookup express layout
+app.set("layout", "layouts/layout");
 app.use(expressEjsLayouts);
-app.use(express.static(path.join(__dirname,"public"))); // style, js, images
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-import mongoose from "mongoose";
 await mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
-db.on("error", (error) => {
-  console.error(error);
-});
-db.once("open", () => {
-  console.log(`Connection stablished to database`);
-});
+db.on("error", (error) => console.error("Database connection error:", error.message));
+db.once("open", () => console.log("Connection established to database"));
 
+// using routes
 app.use("/", indexRouter);
 app.use("/authers", autherRoutes);
 app.use("/books", bookRoutes);
@@ -43,4 +42,3 @@ app.use("/books", bookRoutes);
 app.listen(port, () => {
   console.log(`The Server is running at http://${hostname}:${port}`);
 });
-
