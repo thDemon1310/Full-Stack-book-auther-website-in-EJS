9c5345e09fbf14e628af3aec0bcdf9fb7f83c80d
Utplaksh Mishra <umishra8770@gmail.com>
2025-06-11 19:59:55 +0530

    installed and npm packege that help us to use put and delete methods by post request

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import expressEjsLayouts from "express-ejs-layouts";
 import mongoose from "mongoose";
 import path from "path";
 import { fileURLToPath } from "url";
+import methodOverride from "method-override";
 
 // importing routes
 import indexRouter from "./routes/indexRoute.js";
@@ -28,7 +29,7 @@ app.set("layout", "layouts/layout");
 app.use(expressEjsLayouts);
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(methodOverride('_method'))
 await mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (error) => console.error("Database connection error:", error.message));
